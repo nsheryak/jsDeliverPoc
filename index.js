2e0273f048db9a7d136954bb30a19d9d345c8833
nsheryak <nathan.sheryak@lilly.com>
2023-04-20 11:52:23 -0400

    create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function init() {
+  console.log('Initialized')
+  const value1 = 2
+  const value2 = 3
+  const value3 = value1 + value2
+
+  console.log(value1, value2, value3)
+}
+
+init()
+
