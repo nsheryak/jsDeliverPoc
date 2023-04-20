b996d711d9eddd629bbe3a5a384fced49ac6a924
nsheryak <nathan.sheryak@lilly.com>
2023-04-20 12:06:49 -0400

    copy/past from the nintex sdk guide

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// define the component
+export class HelloWorld extends LitElement {
+  
+  static properties = {
+    who: {type: String},
+  };
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        }
+      }
+    };
+  }
+  
+  constructor() {
+    super();
+    this.who = 'World';
+  }
 
-function init() {
-  console.log('Initialized')
-  const value1 = 2
-  const value2 = 3
-  const value3 = value1 + value2
-
-  console.log(value1, value2, value3)
+  render() {
+    return html`<p>Hello ${this.who}<p/>`;
+  }
 }
 
-init()
+// registering the web component
+const elementName = 'hello-world';
+customElements.define(elementName, HelloWorld);
 
